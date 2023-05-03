fe4433af930c14a1e593422fa17f2795331437cd
igorekSSEUdet <korablev_sseu@mail.ru>
2023-05-03 19:08:27 +0400

    resolve conflicts

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable int userId) {
-        log.debug(String.format("Получен запрос DELETE users/%d", userId));
+        log.debug( String.format("Получен запрос DELETE users/%d", userId));
         userService.deleteUser(userId);
     }
 
